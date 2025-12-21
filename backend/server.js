83e599b16b20267a2f925fcf723dbfe2ffd971d9
Kundan Kumar Singh <kundankumarsingh2005@gmail.com>
2025-12-21 12:52:24 +0530

    forntend url added

--- backend/server.js
+++ backend/server.js
@@ -86,7 +86,7 @@ app.get("/api/health", (req, res) => {
 app.use(errorHandler);
 
 // ================= START =================
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 server.listen(PORT, () => {
   console.log(`🚀 Server running on ${PORT}`);
