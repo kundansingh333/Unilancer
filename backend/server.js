06ea9cc2e1ab90233dda8f26e28df03f06544dce
Kundan Kumar Singh <kundankumarsingh2005@gmail.com>
2025-12-21 11:27:28 +0530

    cors modified

--- backend/server.js
+++ backend/server.js
@@ -21,20 +21,20 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 // ✅ VERY IMPORTANT: Allow socket.io preflight
-// app.use(
-//   cors({
-//     origin: process.env.FRONTEND_URL || "http://localhost:5173",
-//     credentials: true,
-//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//   })
-// );
-
 app.use(
   cors({
-    origin: true, // allow all origins
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
 );
+
+// app.use(
+//   cors({
+//     origin: true, // allow all origins
+//     credentials: true,
+//   })
+// );
 
 app.use("/api/upload", require("./routes/upload"));
 
