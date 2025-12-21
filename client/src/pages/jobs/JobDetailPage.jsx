aaf878c9c02d7cf2ce8eb9924217b0642a40e69e
Kundan Kumar Singh <kundankumarsingh2005@gmail.com>
2025-12-21 11:14:07 +0530

    Fix case-sensitive import for BookmarkButton

--- client/src/pages/jobs/JobDetailPage.jsx
+++ client/src/pages/jobs/JobDetailPage.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import api from "../../api/client";
 import useAuthStore from "../../store/authStore";
-import BookmarkButton from "./BookMarkButton";
+import BookmarkButton from "./BookmarkButton";
 
 const JobDetailPage = () => {
   const { id } = useParams();
