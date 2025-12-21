da9d4527ed88c572ebaa3ee7af70930570c5b5ef
Kundan Kumar Singh <kundankumarsingh2005@gmail.com>
2025-12-21 09:21:06 +0530

    cors problems solved temporarily

--- backend/server.js
+++ backend/server.js
@@ -21,13 +21,21 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 // ✅ VERY IMPORTANT: Allow socket.io preflight
+// app.use(
+//   cors({
+//     origin: process.env.FRONTEND_URL || "http://localhost:5173",
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//   })
+// );
+
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: true, // allow all origins
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
 );
+
 app.use("/api/upload", require("./routes/upload"));
 
 // ================= SOCKET.IO =================
